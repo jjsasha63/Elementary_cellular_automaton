523e84c51707d5da223c0b10108f6f4c49b34a65
jjsasha63 <jjsasha63@gmail.com>
2021-10-25 08:53:09 +0200

    Initial commit

---
+++ src/main/java/com/red/prr1/Board.java
@@ -0,0 +1,112 @@
+package com.red.prr1;
+
+import javafx.geometry.Point2D;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.transform.Affine;
+import javafx.scene.transform.NonInvertibleTransformException;
+
+
+public class Board extends VBox {
+
+    private Button next,res;
+    private ChoiceBox rule;
+    private Canvas canvas;
+    private Affine affine;
+    private int drawMode = 1;
+    private int width = 100;
+    private int heigth = 100;
+
+    private Simulation simulation;
+
+
+    public Board() {
+
+
+        next = new Button("next");
+        next.setMaxSize(600,10);
+
+        this.next.setOnAction(actionEvent -> {
+                    simulation.step(rule.getSelectionModel().getSelectedIndex());
+                    out();
+        });
+        res = new Button("reset");
+        res.setMaxSize(600,10);
+        this.res.setOnAction(actionEvent -> {
+            simulation.reset();
+            out();
+        });
+        rule = new ChoiceBox();
+        rule.setMaxSize(600,10);
+        for(int i=0;i<256;i++) rule.getItems().add(i);
+
+        canvas = new Canvas(600,650);
+        this.canvas.setOnMousePressed(this::handleDraw);
+        this.canvas.setOnMouseDragged(this::handleDraw);
+
+        this.setOnKeyPressed(this::onKeyPressed);
+
+        this.getChildren().addAll(this.next,this.res,this.rule,this.canvas);
+        this.affine = new Affine();
+        this.affine.appendScale(600 / heigth, 750 / width);
+        this.simulation = new Simulation(width,heigth);
+    }
+
+    private void onKeyPressed(KeyEvent keyEvent) {
+        if (keyEvent.getCode() == KeyCode.D) {
+            this.drawMode = 1;
+            System.out.println("Draw");
+        } else if (keyEvent.getCode() == KeyCode.E) {
+            this.drawMode = 0;
+            System.out.println("Erase");
+        }
+    }
+
+    private void handleDraw(MouseEvent event) {
+        double mouseX = event.getX();
+        double mouseY = event.getY();
+        try {
+            Point2D simCoord = this.affine.inverseTransform(mouseX, mouseY);
+            int simX = (int) simCoord.getX();
+            int simY = (int) simCoord.getY();
+            this.simulation.set(simX, simY, drawMode);
+            out();
+        } catch (NonInvertibleTransformException e) {
+            System.out.println("Error");
+        }
+    }
+
+    public void out() {
+        GraphicsContext g = this.canvas.getGraphicsContext2D();
+        g.setTransform(this.affine);
+
+        g.setFill(Color.LIGHTGRAY);
+        g.fillRect(0, 0, 450, 450);
+
+        g.setFill(Color.BLACK);
+        for (int x = 0; x < this.simulation.width; x++) {
+            for (int y = 0; y < this.simulation.height; y++) {
+                if (this.simulation.isAlive(x,y)) {
+                    g.fillRect(x, y, 1, 1);
+                }
+            }
+        }
+
+        g.setStroke(Color.GRAY);
+        g.setLineWidth(0.05);
+        for (int x = 0; x <= this.simulation.width; x++) {
+            g.strokeLine(x, 0, x, heigth);
+        }
+
+        for (int y = 0; y <= this.simulation.height; y++) {
+            g.strokeLine(0, y, width, y);
+        }
+    }
+    }
---
+++ src/main/java/com/red/prr1/Simulation.java
@@ -0,0 +1,65 @@
+package com.red.prr1;
+
+
+public class Simulation {
+
+    int width;
+    int height;
+    int[][] board;
+    int cou = 0;
+
+    public Simulation(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.board = new int[width][height];
+    }
+
+    public boolean isAlive(int x,int y){
+        if(this.board[x][y] == 1) return true;
+        else return false;
+    }
+
+    public int[] rules(int index){
+        int[] rules = new int[8];
+            Integer.toBinaryString(index);
+            String result = String.format("%8s",Integer.toBinaryString(index)).replaceAll(" ","0");
+            for (int j = 0; j < 8; j++){
+                rules[j] = result.charAt(j) - '0';
+            }
+            return rules;
+
+    }
+
+    public void step(int index) {
+        int[] rule = rules(index);
+        int[] binary = new int[3];
+        StringBuffer sb = new StringBuffer();
+        for(int i = 0;i<width;i++){
+            if(i==0) binary[0] = this.board[width-1][cou];
+            else binary[0] = this.board[i-1][cou];
+            binary[1] = this.board[i][cou];
+            if(i==width-1) binary[2] = this.board[0][cou];
+            else binary[2] = this.board[i+1][cou];
+            for(int j=0;j<3;j++) sb.append(binary[j]);
+            this.board[i][cou+1] = rule[((Integer.parseInt(sb.toString(),2))-7)*-1];
+            sb.delete(0,sb.length());
+        }
+        cou++;
+    }
+
+    public void reset(){
+        for(int i =0;i<width;i++){
+            for(int j = 0;j<height;j++){
+                this.board[i][j] = 0;
+            }
+        }
+        cou=0;
+    }
+
+    public void set(int x, int y, int state) {
+        this.board[x][y] = state;
+
+    }
+
+
+}
